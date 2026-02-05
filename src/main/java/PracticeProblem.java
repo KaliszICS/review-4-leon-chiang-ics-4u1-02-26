2bb291b7b9b3483dab59ab0dedc96c82719d5d75
leon-chiang-ics-4u1-02-26 <Leon.Chiang@student.tdsb.on.ca>
2026-02-05 15:11:42 +0000

    Solution

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -12,32 +12,66 @@ public class PracticeProblem {
 	}
 
 	public static void q1() {
-		//Write question 1 code here
+		Scanner s = new Scanner(System.in);
+		System.out.print("Input an integer: ");
+		String x = s.nextLine();
+		x=(5+x);
+		int y = Integer.parseInt(x);
+		System.out.println(y+5);
 		
 	}
 
 	public static void q2() {
-		//Write question 2 code here
+		Scanner s = new Scanner(System.in);
+		System.out.print("Input a number: ");
+		String x = s.nextLine();
+		x=(4+x+3);
+		Double y = Double.parseDouble(x);
+		System.out.println(y+3.4);
 		
 	}
 
 	public static void q3() {
-		//Write question 3 code here
+		Scanner s = new Scanner(System.in);
+		System.out.print("Input a boolean: ");
+		String x = s.nextLine();
+		Boolean y = Boolean.parseBoolean(x);
+		System.out.println(y!=true);
 		
 	}
 
 	public static void q4() {
-		//Write question 4 code here
+		Scanner s = new Scanner(System.in);
+		System.out.print("Input an integer: ");
+		String x = s.nextLine();
+		x=(x+3);
+		int y = Integer.parseInt(x);
+		y=(y+2);
+		char z=(char)y;
+		System.out.println(z);
 		
 	}
 
 	public static void q5() {
-		//Write question 5 code here
+		Scanner s = new Scanner(System.in);
+		System.out.print("Input an integer: ");
+		String x = s.nextLine();
+		x=(x+1);
+		int y = Integer.parseInt(x);
+		y=(y/2);
+		double z=(double)y;
+		System.out.println(z);
 		
 	}
 
 	public static void q6() {
-		//Write question 6 code here
+		Scanner s = new Scanner(System.in);
+		System.out.print("Input a number: ");
+		String x = s.nextLine();
+		x=(1+x);
+		double y = Double.parseDouble(x);
+		int z= (int)y;
+		System.out.println(z);
 		
 	}
 
